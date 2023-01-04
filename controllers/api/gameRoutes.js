ab265c95d8fb199349d1c21eb36e2241d93d180c
CECurtiss <curtissclinton@gmail.com>
2023-01-03 20:57:01 -0600

    changed model definition in routes

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Game } = require('../..//models/game')
+const Game = require('../..//models/game')
 
 router.get('/', (req, res) => {
     Game.findAll().then((gameData) => {
