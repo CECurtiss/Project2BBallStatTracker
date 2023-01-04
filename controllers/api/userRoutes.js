d2229e3e23cf410b2e0e6dea05c0a9c16efa2027
CECurtiss <curtissclinton@gmail.com>
2023-01-03 21:26:18 -0600

    wrote login.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -17,7 +17,7 @@ router.post("/", async (req, res) => {
 });
 router.post('/login', async (req, res) => {
     try {
-      const userData = await User.findOne({ where: { email: req.body.email } });
+      const userData = await User.findOne({ where: { username: req.body.username } });
   
       if (!userData) {
         res
