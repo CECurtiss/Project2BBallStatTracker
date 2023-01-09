f700fd097d7ffc355c13f54d058ed2fccb56e61e
CECurtiss <curtissclinton@gmail.com>
2023-01-09 16:05:13 -0600

    fixed route for single player bio and stat info

--- controllers/api/playerBioRoute.js
+++ controllers/api/playerBioRoute.js
@@ -1,15 +1,31 @@
 const router = require("express").Router();
 const Playerbio  = require("../../models/playerbio");
+const PlayerStats = require("../../models/playerStats")
 
-router.get("/:id", (req, res) => {
-  Playerbio.findOne({
+router.get("/:id", async (req, res) => {
+  const getPlayer = await Playerbio.findOne({
     where: {
       id: req.params.id,
-    },
-  }).then((playerData) => {
-    res.json(playerData);
-  });
-});
+    }},{
+      include: [
+        {
+          model: PlayerStats,
+          attributes: [
+            'points',
+            'rebounds',
+            'assists',
+            'steals',
+            'turnovers',
+            'blocks',
+            "personalFouls",
+          ]
+        }
+      ]
+    })
+    const player = getPlayer.get({ plain: true })
+    res.render('singleplayer', { player })
+    
+  })
 
 router.post("/", async (req, res) => {
   try {
--- controllers/api/playerRoutes.js
+++ controllers/api/playerRoutes.js
@@ -1,58 +1,60 @@
-const router = require('express').Router();
-const PlayerStats = require('../../models/playerstats');
+const router = require("express").Router();
+const PlayerStats = require("../../models/playerstats");
 
-router.get('/', async (req,res) => {
-    try {
-        const allPlayerStats = await PlayerStats.findAll()
-        res.status(200).json(allPlayerStats)
-    } catch (err) {
-        res.status(500).json(err)
+router.get("/", async (req, res) => {
+  const allPlayerStats = await PlayerStats.findAll({ 
+    where: {
+      id: req.params.id,
     }
+  })
+  const singleGamePlayers = allPlayerStats.map((gamePlayers) =>
+    gamePlayers.get({ plain: true })
+  );
+  res.render("playerstats", { singleGamePlayers });
 });
 
-router.get('/:id', async (req, res) => {
-    try {
-        const gamePlayers = await PlayerStats.findAll(req.params.gameId,{
-        })
-        const allGamePlayers = gamePlayers.map((aGPlayers) => aGPlayers.get({ plain: true }))
- 
-    res.render('playerstats', { allGamePlayers })
+router.get("/:id", async (req, res) => {
+  try {
+    const gamePlayers = await PlayerStats.findAll(req.params.gameId, {});
+    const allGamePlayers = gamePlayers.map((aGPlayers) =>
+      aGPlayers.get({ plain: true })
+    );
 
-      
-    } catch(err) {
-        res.status(500).json(err)
-    }
-})
+    res.render("playerstats", { allGamePlayers });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-router.post('/', async (req, res) => {
-    try {
-        const newPlayer = await PlayerStats.create({
-         ...req.body,
-        });
-
-        res.status(200).json(newPlayer);
-      } catch (err) {
-        res.status(400).json(err);
-      }
+router.post("/", async (req, res) => {
+  try {
+    const newPlayer = await PlayerStats.create({
+      ...req.body,
     });
 
-router.delete('/:id', async (req, res) => {
-    try {
-        const playerData = await PlayerStats.destroy({
-            where: {
-             id: req.params.id,
-            },
-        });
+    res.status(200).json(newPlayer);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-        if (!playerData) {
-            res.status(404).json({ message: 'Player not found'});
-            return;
-        }
-        
-        res.status(200).json(playerData);
-    }    catch (err) {
-        res.status(500).json(err);
-    }
+router.delete("/:id", async (req, res) => {
+  try {
+    const playerData = await PlayerStats.destroy({
+      where: {
+        id: req.params.id,
+      },
     });
 
-    module.exports = router;
+    if (!playerData) {
+      res.status(404).json({ message: "Player not found" });
+      return;
+    }
+
+    res.status(200).json(playerData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
