68ac6231cbb07406a72e8d8013f346e72a30f94c
CECurtiss <curtissclinton@gmail.com>
2023-01-15 19:23:47 -0600

    tried to add form routes to add new players, games, stats

--- controllers/html/index.js
+++ controllers/html/index.js
@@ -3,11 +3,13 @@ const gamePlayersRoutes = require('./gameplayers')
 const homepageRoutes = require('./homepage');
 const singlePlayerRoutes = require('./singleplayer')
 const addGameRoutes = require('./addgame')
+const createNewPlayerRoutes = require ('./createnewplayer')
 
 router.use('/', homepageRoutes);
 router.use('/gameplayers', gamePlayersRoutes )
 router.use('/singleplayer', singlePlayerRoutes)
 router.use('/addgame', addGameRoutes)
+router.use('/createnewplayer', createNewPlayerRoutes)
 
 
 module.exports = router;
---
+++ public/js/createnewplayer.js
@@ -0,0 +1,41 @@
+const addNewPlayerBioData = async (event) => {
+  event.preventDefault();
+  const newPlayerName = document.querySelector("#newname").value;
+  const newPlayerPosition = document.querySelector("#position").value;
+  const newPlayerAge = document.querySelector("#age").value;
+  const newPlayerHeight = document.querySelector("#height").value;
+  const newPlayerWeight = document.querySelector("#weight").value;
+  const newPlayerCollege = document.querySelector("#college").value;
+
+  if (
+    newPlayerName &&
+    newPlayerPosition &&
+    newPlayerAge &&
+    newPlayerHeight &&
+    newPlayerWeight &&
+    newPlayerCollege
+  ) {
+    const response = await fetch("/api/singleplayer", {
+      method: "POST",
+      body: JSON.stringify({
+        newPlayerName,
+        newPlayerPosition,
+        newPlayerAge,
+        newPlayerHeight,
+        newPlayerWeight,
+        newPlayerCollege,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    console.log(gdate);
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("There was an error");
+    }
+  }
+};
+
+document
+  .querySelector("#addplayerbiodata")
+  .addEventListener("click", addNewPlayerBioData);
